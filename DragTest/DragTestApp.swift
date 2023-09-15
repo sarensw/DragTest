3ef497708c779d041098eb72aeaafa1472b54c8f
Arenswald Stephan <stephan.arenswald@mercedes-benz.com>
2023-09-15 04:08:48 +0200

    Initial Commit

---
+++ DragTest/DragTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragTestApp.swift
+//  DragTest
+//
+//  Created by Arenswald, Stephan (059) on 15.09.23.
+//
+
+import SwiftUI
+
+@main
+struct DragTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
